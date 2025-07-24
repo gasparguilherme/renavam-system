f2114bd9c91657195f6bcd146f0725bb0d8b39de
Guilherme Gaspar <guilhermegaspar474@gmail.com>
2025-07-24 17:50:05 -0300

    excluindo arquivos update para uma melhor abordagem

--- internal/handlers/person/usecase.go
+++ internal/handlers/person/usecase.go
@@ -7,6 +7,4 @@ import (
 
 type Usecase interface {
 	CreatePerson(name, cpf string, date_of_birth time.Time, phone, email string) (*entities.Person, error)
-	UpdatePerson(id int, newName string, neWCPF string, newDateOfBirth time.Time,
-		newPhone string, newEmail string) error
 }
--- internal/usecase/person/repository.go
+++ internal/usecase/person/repository.go
@@ -4,5 +4,4 @@ import "renavam-system/internal/entities"
 
 type Repository interface {
 	SavePerson(data entities.Person) (int, error)
-	UpdatePerson(data entities.Person) error
 }
