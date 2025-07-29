d03fed37a7aba9cc9a2a91c93af7404fc1f1e897
Guilherme Gaspar <guilhermegaspar474@gmail.com>
2025-07-29 11:43:30 -0300

    melhorias na branch local

--- internal/api/http.go
+++ internal/api/http.go
@@ -5,6 +5,7 @@ import "net/http"
 func StartApp(personHandler PersonHandler) {
 	mux := http.NewServeMux()
 	mux.Handle("POST /create-person", http.HandlerFunc(personHandler.CreatePersonHandler))
+	mux.Handle("PUT /edit-person/{id}", http.HandlerFunc(personHandler.UpdatePersonHandler))
 
 	http.ListenAndServe(":6161", mux)
 }
