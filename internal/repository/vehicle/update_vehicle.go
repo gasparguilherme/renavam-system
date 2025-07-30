b4752656b04bbad47726351d1a412ad4048f2c72
Guilherme Gaspar <guilhermegaspar474@gmail.com>
2025-07-30 20:08:27 -0300

    criacao da func UpdateVehicle na camada usecase

--- internal/repository/vehicle/update_vehicle.go
+++ internal/repository/vehicle/update_vehicle.go
@@ -6,19 +6,27 @@ import (
 	"renavam-system/internal/dto"
 )
 
-func (r Repository) UpdateVehicle(data dto.UpdateVehicleInput) (int, error) {
+func (r Repository) UpdateVehicle(data dto.UpdateVehicleInput) error {
 	query := `UPDATE vehicles 
-	SET 
-		color = $1, 
-		personID = $2,
-		WHERE id = $3,
-		RETURNING id = $4;
+		SET 
+			color = $1, 
+			personID = $2
+		WHERE id = $3
+		RETURNING id;
 	`
-	var id int
-	err := r.connectionInstance.QueryRow(context.TODO(), query, data.Color, data.PersonID).Scan(&id)
-	if err != nil {
-		return 0, fmt.Errorf("failure to update person: %w (query: %s)", err, query)
 
+	var id int
+	err := r.connectionInstance.QueryRow(
+		context.TODO(),
+		query,
+		data.Color,
+		data.PersonID,
+		data.ID,
+	).Scan(&id)
+
+	if err != nil {
+		return fmt.Errorf("failure to update vehicle: %w (query: %s)", err, query)
 	}
-	return id, nil
+
+	return nil
 }
--- internal/usecase/vehicle/repository.go
+++ internal/usecase/vehicle/repository.go
@@ -1,7 +1,11 @@
 package vehicle
 
-import "renavam-system/internal/entities"
+import (
+	"renavam-system/internal/dto"
+	"renavam-system/internal/entities"
+)
 
 type Repository interface {
 	SaveVehicle(data entities.Vehicle) (int, error)
+	UpdateVehicle(data dto.UpdateVehicleInput) error
 }
